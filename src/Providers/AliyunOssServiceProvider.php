ab57dbdb569782a0c1a6ec44329182b2dfde73c2
wangxuan <wangxuan@qgpic.com>
2016-12-14 15:30:19 +0800

    add aliyun oss service provider

---
+++ src/Providers/AliyunOssServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Fuguevit\Storage\Providers;
+
+use Fuguevit\Storage\AliyunOssAdapter;
+use Fuguevit\Storage\Plugins\PutFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use OSS\OssClient;
+
+class AliyunOssServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        Storage::extend('oss', function($app, $config) {
+            $accessId = $config['access_id'];
+            $accessKey = $config['access_key'];
+            $endPoint = $config['endpoint'];
+            $bucket = $config['bucket'];
+            $isCName = $config['isCName'];
+            $debug = $config['debug'];
+            
+            $client = new OssClient($accessId, $accessKey, $endPoint, $isCName);
+            $adapter = new AliyunOssAdapter($client, $bucket);
+            $filesystem = new Filesystem($adapter);
+            $filesystem->addPlugin(new PutFile());
+            
+            return $filesystem;
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        
+    }
+}
