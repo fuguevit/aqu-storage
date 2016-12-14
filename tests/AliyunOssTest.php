397401b246728c3c538931d7c42942f0a1f829d1
wangxuan <wangxuan@qgpic.com>
2016-12-14 18:54:41 +0800

    setup phpunit test

--- tests/AliyunOssTest.php
+++ tests/AliyunOssTest.php
@@ -2,6 +2,9 @@
 
 namespace Fuguevit\Storage\Tests;
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
 class AliyunOssTest extends TestCase
 {
     /**
@@ -19,6 +22,8 @@ class AliyunOssTest extends TestCase
      */
     public function test_it_can_push_file_to_oss()
     {
+        Storage::put('samples/sample_img.jpg', new File(__DIR__.'/assets/sample_img.jpg'));
+
         $this->assertTrue(true);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,15 +2,20 @@
 
 namespace Fuguevit\Storage\Tests;
 
+use Dotenv\Dotenv;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
+    protected $ossConfList;
+    
     /**
      * {@inheritdoc}
      */
     public function setUp()
     {
+        $this->initDotEnv();
+        $this->initOssConfig();
         parent::setUp();
     }
 
@@ -20,6 +25,32 @@ class TestCase extends OrchestraTestCase
     public function getEnvironmentSetUp($app)
     {
         parent::getEnvironmentSetUp($app);
+        
+        $app['path.base'] = __DIR__ . '/../src';
+        $app['config']->set('filesystems.default', 'oss');
+        $app['config']->set('filesystems.disks.oss', $this->ossConfList);
+    }
+
+    protected function initDotEnv()
+    {
+        $dotenv = new Dotenv(__DIR__.'/../');
+        $dotenv->overload();
+    }
+
+    /**
+     * Init Oss Configuration.
+     */
+    protected function initOssConfig()
+    {
+        $this->ossConfList = [
+            'driver'      => 'oss',
+            'access_id'   => env('OSS_ACCESS_ID', ''),
+            'access_key'  => env('OSS_ACCESS_KEY', ''),
+            'bucket'      => env('OSS_BUCKET', ''),
+            'endpoint'    => env('OSS_ENDPOINT', ''),
+            'isCName'     => env('OSS_IS_CNAME', ''),
+            'debug'       => env('OSS_DEBUG', '')
+        ];
     }
 
     /**
